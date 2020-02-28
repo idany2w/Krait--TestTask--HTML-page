0d0e7ee36194d0075bb66cffad9cef139065d865
idany2w <54327037+idany2w@users.noreply.github.com>
2020-02-28 22:18:55 +0300

    initial

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+window.addEventListener('scroll', function(){
+	if (window.pageYOffset !== 0){
+		document.querySelector('nav.navbar').classList.add('scrolled');
+	} else {
+		let toggler = document.querySelector('nav.navbar .navbar-toggler.collapsed');
+		
+		if(toggler){
+			document.querySelector('nav.navbar').classList.remove('scrolled');
+		};
+	};
+});
+
+document.querySelector('nav.navbar .navbar-toggler').addEventListener('click', function(e){
+	let isCollapsed = e.target.closest('.collapsed'),
+		isScrol = window.pageYOffset !== 0;
+
+	if(!isScrol){
+		if(isCollapsed){
+			document.querySelector('nav.navbar').classList.add('scrolled');
+		} else{
+			document.querySelector('nav.navbar').classList.remove('scrolled');
+		}
+	};
+},true)
+
+/* Swiper slider */
+var swiper = new Swiper('.swiper-container', {
+	slidesPerView: 'auto',
+	initialSlide: 3,
+	centeredSlides: true,
+	grabCursor: true,
+	setTransition: 300,
+	watchOverflow: true,
+	effect: 'coverflow',
+	coverflowEffect: {
+		rotate: 0,
+		stretch: 150,
+		depth: 200,
+		modifier: 1,
+		slideShadows: false
+	},
+	navigation: {
+		nextEl: '.swiper-next',
+		prevEl: '.swiper-prev',
+	}
+});
